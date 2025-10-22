57d20d70424add97bbfaf26652446826efef294f
nielassis <nielvitorba@gmail.com>
2025-10-22 19:21:44 -0300

    fix: swagger ui on production

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,23 +1,36 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
+import path from "path";
+
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "PagLemon API",
+    version: "1.0.0",
+    description: "API de pagamentos simples com logs de transações",
+  },
+};
 
 const options: swaggerJsdoc.Options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "PagLemon API",
-      version: "1.0.0",
-      description: "API de pagamentos simples com logs de transações",
-    },
-  },
-  apis: [`${process.cwd()}/src/routes/*.ts`],
+  definition: swaggerDefinition,
+  apis:
+    process.env.NODE_ENV === "production"
+      ? [path.join(__dirname, "../routes/*.js")]
+      : [path.join(__dirname, "../routes/*.ts")],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
 
 export function setupSwagger(app: Express) {
-  app.get("/swagger.json", (req, res) => res.json(swaggerSpec));
+  app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, { explorer: true }),
+  );
 
-  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.get("/swagger.json", (req, res) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+  });
 }
