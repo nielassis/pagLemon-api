9cdcf541109402fdc21fb272b8caaeab7ad55615
nielassis <nielvitorba@gmail.com>
2025-10-22 20:35:46 -0300

    fix: trying to make swagger doc ui work on vercel serveless function

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,4 @@
-import app from "../src/app";
+import app from "../app";
 
 export default function handler(req: any, res: any) {
   return app(req, res);
--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -1,23 +1,17 @@
 import swaggerJsdoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
-import path from "path";
-
-const swaggerDefinition = {
-  openapi: "3.0.0",
-  info: {
-    title: "PagLemon API",
-    version: "1.0.0",
-    description: "API de pagamentos simples com logs de transações",
-  },
-};
 
 const options: swaggerJsdoc.Options = {
-  definition: swaggerDefinition,
-  apis:
-    process.env.NODE_ENV === "production"
-      ? [path.join(__dirname, "../routes/*.js")]
-      : [path.join(__dirname, "../routes/*.ts")],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "PagLemon API",
+      version: "1.0.0",
+      description: "API de pagamentos simples com logs de transações",
+    },
+  },
+  apis: ["./src/routes/*.ts", "./dist/routes/*.js"],
 };
 
 const swaggerSpec = swaggerJsdoc(options);
@@ -26,10 +20,13 @@ export function setupSwagger(app: Express) {
   app.use(
     "/docs",
     swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, { explorer: true }),
+    swaggerUi.setup(swaggerSpec, {
+      explorer: true,
+      customCss: ".swagger-ui .topbar { display: none }",
+    }),
   );
 
-  app.get("/swagger.json", (req, res) => {
+  app.get("/swagger.json", (_, res) => {
     res.setHeader("Content-Type", "application/json");
     res.send(swaggerSpec);
   });
