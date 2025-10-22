5447c6ebbb34b3326ce53cc22e6f797e16ead45b
nielassis <nielvitorba@gmail.com>
2025-10-22 18:56:08 -0300

    feat(docs): document api

---
+++ src/docs/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+
+const options: swaggerJsdoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "PagLemon API",
+      version: "1.0.0",
+      description: "API de pagamentos simples com logs de transações",
+    },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export function setupSwagger(app: Express) {
+  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
