efe464f009dc840cbd2d66668ea99529e3b4df4f
nielassis <nielvitorba@gmail.com>
2025-10-22 19:26:35 -0300

    fix: little tweak

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,7 @@
 import app from "../app";
 
 export default async function handler(req: any, res: any) {
-  await new Promise<void>((resolve) => app(req, res, () => resolve()));
+  await new Promise<void>((resolve) => {
+    app(req, res, () => resolve());
+  });
 }
